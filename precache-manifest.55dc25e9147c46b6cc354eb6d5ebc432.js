ad785667dab04f0d23743beb49b6ee723eddc175
Rushabh Parekh <rushabh.parekh.11@gmail.com>
2019-10-15 17:09:09 -0400

    Updates

---
+++ precache-manifest.55dc25e9147c46b6cc354eb6d5ebc432.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d82d65a96311488ed3daadd9f55ba20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ed9d747b2174294e7af",
+    "url": "/static/css/main.b3f35b52.chunk.css"
+  },
+  {
+    "revision": "b367951cda36139501f6",
+    "url": "/static/js/2.a98d282d.chunk.js"
+  },
+  {
+    "revision": "7ed9d747b2174294e7af",
+    "url": "/static/js/main.133066ee.chunk.js"
+  },
+  {
+    "revision": "6218a4003c17a80ff246",
+    "url": "/static/js/runtime-main.77a10221.js"
+  },
+  {
+    "revision": "a0e83bac03fbb8570b0c2ae9ba531f20",
+    "url": "/static/media/placeHolderImage.a0e83bac.svg"
+  },
+  {
+    "revision": "cfe982bf7c5ae915cd07936439cff780",
+    "url": "/static/media/wealthsimpleLogoGreyBackground.cfe982bf.png"
+  }
+]);
